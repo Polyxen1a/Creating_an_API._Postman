8c3fc2f31889cf49152b1ca727c80c6823d2d115
Polyxen1a <maslowa@inbox.ru>
2023-02-04 22:22:54 +0300

    Initial commit

---
+++ src/test/java/com/skypro/creating_an_api_postman/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.creating_an_api_postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
